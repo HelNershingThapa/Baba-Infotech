2c2089206ef77bcc4f01163991607e754d931f7a
helnershingthapa <helnershingthapa@gmail.com>
2019-08-03 22:44:03 +0545

    minor changes

--- src/components/Navigation-bar.vue
+++ src/components/Navigation-bar.vue
@@ -1,27 +1,17 @@
 <template>
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
     <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Baba Infotech & General Traders (P) Ltd.</a>
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <img src="../assets/images/lgo.png">
+      </a>
       <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         Menu
         <i class="fas fa-bars"></i>
       </button>
       <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav text-uppercase ml-auto">
+        <ul class="navbar-nav text-uppercase ml-auto" v-for="item in navItems" v-bind:key="item.title">
           <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#services">Services</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#team">Team</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            <a class="nav-link js-scroll-trigger" :href="`#${item.place}`">{{item.title}}</a>
           </li>
         </ul>
       </div>
@@ -30,7 +20,31 @@
 </template>
 
 <script>
+import Detail from '@/constants/detail';
+
 export default {
+  data() {
+    return {
+      navItems: [{
+        title: 'Services',
+        place: 'services'
+      },
+      {
+        title: 'Portfolio',
+        place: 'portfolio'
+      },
+      {
+        title: 'Portfolio',
+        place: 'portfolio'
+      },
+      {
+        title: 'Services',
+        place: 'services'
+      }
+      ]
+    }
+
+  }
 
 }
 </script>
---
+++ src/constants/detail.js
@@ -0,0 +1,8 @@
+const WEB_DETAIL = {
+  basic: {
+    title: 'gsagagj',
+    meta: 'kjkj'
+  }
+};
+
+export default WEB_DETAIL;
